0f51e97887e4f2023bbbdea6314c0a788a8285d5
DavidLynch26 <lynchda78@gmail.com>
2022-11-02 18:25:27 -0500

    html and css file made, js starter code added

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.onload = function(){
+
+    var searchButton = document.getElementsByClassName("btn")[0];
+
+    searchButton.addEventListener("click", function(){
+
+    });
+}
