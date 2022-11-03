5381bb24533c033a9011c32d85a43578a0359a31
DavidLynch26 <lynchda78@gmail.com>
2022-11-03 11:18:27 -0500

    fetch and alert added to js, bg color of html changed

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 'use strict';
 
 window.onload = function(){
-
     var searchButton = document.getElementsByClassName("btn")[0];
 
-    searchButton.addEventListener("click", function(){
+    
 
+    searchButton.addEventListener("click", function(){
+        fetch("http://localhost/info2180-lab4/superheroes.php")
+            .then(response => response.text())
+            .then(data => {
+                alert(data);
+            })
     });
 }
