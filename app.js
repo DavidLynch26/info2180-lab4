47ca58d9b75375a72e9ef4ffc2d8cb61c93b17ae
DavidLynch26 <lynchda78@gmail.com>
2022-11-07 21:22:45 -0500

    added styling, result swapping not functional

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 window.onload = function(){
     var searchButton = document.getElementsByClassName("btn")[0];
-    var superHeroName = document.getElementById("superHeroName");
+    document.getElementById("superHeroName").innerHTML.trim();
     var result = document.getElementById("result");
 
-    searchButton.addEventListener("click", function(){
-        fetch("http://localhost/info2180-lab4/superheroes.php")
+    searchButton.addEventListener("click", function(event){
+        event.preventDefault();
+        fetch("superheroes.php")
             .then(response => response.text())
             .then(data => {
-                alert(data);
+                console.log(data);
             })
+        // document.getElementsByTagName("form")[0].submit();
     });
 }
